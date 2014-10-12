d3ae2ebf15c6f5c2932fa803dc5078faab41303a
Savannah Henderson <savannahjune@gmail.com>
2014-10-11 18:13:32 -0700

    finished second function

--- skills2.py
+++ skills2.py
@@ -18,7 +18,7 @@ def count_unique(string1):
     		uniquedict[word] = 1
     	else: 
     		uniquedict[word] += 1
-    print uniquedict
+    return uniquedict
 count_unique(string1)
 
 """
@@ -26,6 +26,37 @@ Given two lists, (without using the keywords 'if __ in ____' or the method 'inde
 return a list of all common items shared between both lists
 """
 def common_items(list1, list2):
+    uniquedict1 = {}
+    uniquedict2 = {}
+    commondict = {}
+    for item in list1:
+    	if item not in uniquedict1:
+    		uniquedict1[item] = 1
+    	else: 
+    		uniquedict1[item] += 1
+ 
+    for item in list2:
+    	if item not in uniquedict2:
+    		uniquedict2[item] = 1
+    	else: 
+    		uniquedict2[item] += 1
+
+    for key in uniquedict1:
+    	if key in uniquedict2:
+    		commondict[key] = 1
+    	else:
+    		continue
+    return commondict.keys()
+common_items(list1, list2)
+
+
+
+"""
+Given two lists, (without using 'if __ in ____' or 'index'
+return a list of all common items shared between both lists. This time,
+use a dictionary as part of your solution.
+"""
+def common_items2(list1, list2):
     uniquedict1 = {}
     uniquedict2 = {}
     commondict = {}
@@ -47,24 +78,22 @@ def common_items(list1, list2):
     		commondict[key] = 1
     	else:
     		continue
-    print commondict.keys()
+    return commondict.keys()
 common_items(list1, list2)
-
-
-
-"""
-Given two lists, (without using 'if __ in ____' or 'index'
-return a list of all common items shared between both lists. This time,
-use a dictionary as part of your solution.
-"""
-def common_items2(list1, list2):
-    pass
 
 """
 Given a list of numbers, return list of number pairs that sum to zero
 """
 def sum_zero(list1):
-    pass
+    sumzerolist = []
+    i = 0
+    for i in list1:
+    	if number[i] + number[i +1] == 0:
+    		sumzerolist.append(number[i], number[i+1])
+    	else:
+    		i += 1
+    print sumzerolist
+sum_zero(list1)
 
 """
 Given a list of words, return a list of words with duplicates removed
